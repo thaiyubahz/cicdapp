779e8d7e50d5df07b6dd3e1898a59dee6db97f4d
thaiyubahz <136082155+thaiyubahz@users.noreply.github.com>
2026-06-09 00:11:20 +0530

    Updated project features

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello CI/CD');
+    res.send('HELLO THIS IS THAIYUBAHZ ');
 });
 
 app.get('/health', (req, res) => {
